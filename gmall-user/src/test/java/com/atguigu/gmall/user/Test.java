21b45335535840918128d60ac449a05e516c13f5
yoohone <yoohonenb@163.com>
2020-08-10 21:01:50 +0800

    新增一些电商项目测试增加了几个测试类

--- gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
+++ gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
@@ -3,6 +3,10 @@ package com.atguigu.gmall.user;
 public class Test {
 
     public static void main(String[] args) {
+
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
         System.out.println("我是你大爷！！！");
     }
 }
