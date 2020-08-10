8d0a8d2787ede6d6bab34e46498d9eec24dcd5e9
yoohone <yoohonenb@163.com>
2020-08-10 21:15:32 +0800

    新增加一些打印类

--- gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
+++ gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
@@ -4,5 +4,25 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
+        System.out.println("我是你大爷！！！");
     }
 }
