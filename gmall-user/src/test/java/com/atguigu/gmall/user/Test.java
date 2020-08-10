9e3f60b216e20faccc05de4d89624d9773514820
yoohone <yoohonenb@163.com>
2020-08-10 21:45:37 +0800

    删除所有的测试代码

--- gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
+++ gmall-user/src/test/java/com/atguigu/gmall/user/Test.java
@@ -4,17 +4,9 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-        System.out.println("我是你大爷！！！");
-    
-  
+
+
+
 
     }
 }
